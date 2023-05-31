5c98d9a8327e973f31af8c00437911956153fbf2
Faizan <faizan.mansur87@gmail.com>
2023-05-31 16:32:11 +0500

    Removed unnecessary code and created HeaderDesign component

---
+++ src/components/HeaderDesign/HeaderDesign.js
@@ -0,0 +1,11 @@
+import './HeaderDesign.css'
+
+const HeaderDesign = () => {
+  return (
+    <div className="Container">
+      <div className="HeaderDesign"></div>
+    </div>
+  )
+}
+
+export default HeaderDesign
